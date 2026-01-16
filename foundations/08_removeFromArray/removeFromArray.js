ee06396adf16814ba9ed1a8512b3b8f4e7e30e6d
Christopher G <135375609+AI-Ocko@users.noreply.github.com>
2026-01-16 13:19:14 -0800

    Passed tests!

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...num) {
+  let result = array.filter((item) => {
+    return !num.includes(item);
+  })
+  return result;
 };
+
+console.log(removeFromArray([1, 2, 3, 4], 3))
 
 // Do not edit below this line
 module.exports = removeFromArray;
