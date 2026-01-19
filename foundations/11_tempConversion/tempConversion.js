3958f5cf793959296bad8ec3401800448142ca6a
Christopher G <135375609+AI-Ocko@users.noreply.github.com>
2026-01-18 21:19:34 -0800

    Passed the tests o7

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,8 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(input) {
+  let convertedNumber = ((input - 32) * (5 / 9));
+  if (convertedNumber === 0) {
+    return 0;
+  }
+  else {
+    return +Number.parseFloat(convertedNumber).toFixed(1)
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(input) {
+  let convertedNumber = (input * (9 / 5) + 32);
+  return +Number.parseFloat(convertedNumber).toFixed(1)
 };
+
+// console.log(convertToCelsius(32))
+// console.log(convertToCelsius(100))
 
 // Do not edit below this line
 module.exports = {
