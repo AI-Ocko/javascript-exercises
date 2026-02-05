725784f9bf150251a14d3843816e0d76535e45a9
Christopher G <135375609+AI-Ocko@users.noreply.github.com>
2026-02-04 17:47:56 -0800

    passing tests

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,30 @@
-const fibonacci = function() {
+const fibonacci = function (number) {
+  let n = Number(number);
 
+  if (n < 0) {
+    return "OOPS";
+
+  } else if (n == 0) {
+    return 0;
+
+  } else if (n == 1) {
+    return 1;
+
+  } else if (n == 2) {
+    return 1;
+
+  } else {
+    let firstNum = 1;
+    let secondNum = 1;
+    let result = firstNum + secondNum;
+
+    for (let i = 3; i < n; i++) {
+      secondNum = firstNum;
+      firstNum = result;
+      result = firstNum + secondNum;
+    }
+    return result;
+  }
 };
 
 // Do not edit below this line
