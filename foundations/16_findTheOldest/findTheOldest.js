eded5c82f18d2211e81867f86fa05a5d73c81179
Christopher G <135375609+AI-Ocko@users.noreply.github.com>
2026-02-04 22:09:21 -0800

    doen

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  const getAge = function (person) {
+    const currentYear = new Date().getFullYear();
+    const deathYear = person.yearOfDeath || currentYear;
+    return deathYear - person.yearOfBirth;
+  };
+  return people.reduce((oldest, current) => {
+    const oldestAge = getAge(oldest);
+    const currentAge = getAge(current);
+    return currentAge > oldestAge ? current : oldest;
+  });
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
